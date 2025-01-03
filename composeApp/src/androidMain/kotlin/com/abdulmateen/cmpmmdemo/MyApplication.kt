51978b100d34630d0fc147fe3763d8d37b42e06d
Abdulmateen <abdulmateen6.ch@gmail.com>
2025-01-03 09:47:14 +0500

    SettingUp koin and ktor

---
+++ composeApp/src/androidMain/kotlin/com/abdulmateen/cmpmmdemo/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.abdulmateen.cmpmmdemo
+
+import android.app.Application
+import com.abdulmateen.cmpmmdemo.core.common.di.initKoin
+import org.koin.android.ext.koin.androidContext
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initKoin {
+            androidContext(this@MyApplication)
+        }
+    }
+
+}
--- composeApp/src/iosMain/kotlin/com/abdulmateen/cmpmmdemo/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/abdulmateen/cmpmmdemo/MainViewController.kt
@@ -1,5 +1,10 @@
 package com.abdulmateen.cmpmmdemo
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.abdulmateen.cmpmmdemo.core.common.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) { App() }
